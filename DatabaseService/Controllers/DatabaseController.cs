be99a74cd334d4c68f853cccd087ce26ce8b0ff7
Manuel Reinhart <manuel.reinhart@edu.campus02.at>
2020-01-01 20:57:31 +0100

    create service architecture and proxy

---
+++ DatabaseService/Controllers/DatabaseController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Common.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace DatabaseService.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class DatabaseController : ControllerBase
+    {       
+   
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Question>>> GetAsync()
+        {            
+            var questions = await CosmosDbClient.Singleton.Get<Question>();
+            return questions;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<string>> GetAsync(string id)
+        {
+            dynamic item = new
+            {
+                id = id,
+                Name = "test",
+                age = 4
+            };            
+            await CosmosDbClient.Singleton.Insert(item);
+
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost("insert")]
+        public void Post([FromBody] string value)
+        {
+            List<object> questions = JsonConvert.DeserializeObject<List<object>>(value);
+            
+
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ QuestionCreator/Controllers/QuestionCreatorController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Common.Tools;
+
+namespace QuestionCreator.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class QuestionCreatorController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> GetAsync()
+        {
+            var keyVaultUri = await KeyVault.Singleton.GetSecretByKey("asg");
+            return new string[] { "value1", "value2", keyVaultUri };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<string>> GetAsync(string id)
+        {
+            var secret = await KeyVault.Singleton.GetSecretByKey(id);
+
+            if (secret == null)
+                return "secret not found";
+
+            return secret;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+    }
+}
